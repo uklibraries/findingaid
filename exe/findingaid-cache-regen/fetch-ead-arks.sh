b64124696bbcb9ae4674154e18bea3cd9d0f9f4d
Neal <neal.powers@outlook.com>
2026-01-15 08:30:17 -0500

    make arks file ephemeral. Add some additional logging. Fail early.

--- exe/findingaid-cache-regen/fetch-ead-arks.sh
+++ exe/findingaid-cache-regen/fetch-ead-arks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 # The environment should provide these variables
 # SOLR_URL: e.g. https://solrhost.example.com/solr/select
@@ -6,6 +7,10 @@
 
 COUNT_URL="${SOLR_URL}?wt=json&fl=id&fq=format:collections"
 numFound=$(curl -s "$COUNT_URL" | jq -r .response.numFound)
+echo "Found $numFound collections"
 
+echo "Fetching ids"
 EADS_URL="${SOLR_URL}?wt=json&fl=id&fq=format:collections&rows=${numFound}"
 curl -s "$EADS_URL" | jq -r '.response.docs[] | .id' > "$ARKS_FILE"
+
+echo "IDs written to $ARKS_FILE"
