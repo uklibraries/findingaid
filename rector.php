14643c1adfc07fc17ceb62d11bfb5a4784b7f2d3
Neal <neal.powers@outlook.com>
2026-09-09 11:45:50 -0400

    use composer based set for rector; fixes #86

--- rector.php
+++ rector.php
@@ -10,6 +10,7 @@ return RectorConfig::configure()
 
 # Ensures Rector runs with whatever PHP version it finds in composer.json
 ->withPhpSets()
+->withComposerBased(phpunit: true)
 ->withTypeCoverageLevel(0)
 ->withDeadCodeLevel(0)
 ->withCodeQualityLevel(0);
